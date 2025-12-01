8ac5d2c6e431e0b1b87ab3fe572ba28c125e3353
callumselv <callum.selvadurai@hotmail.co.uk>
2025-12-01 12:36:21 +0000

    added a function to plot ideal measurement statistics against those influenced by a finite number of shots

--- src/cubeit/notebook_functions.py
+++ src/cubeit/notebook_functions.py
@@ -11,6 +11,9 @@ from cubeit import (
 )
 from cubeit.visualisation import plot_bloch_sphere, plot_circuit, simulate_measurements
 
+from cubeit.DMs import DensityMatrix2Qubit as DM2
+from cubeit.DMs import DensityMatrix1Qubit as DM1
+
 import os
 
 #Import plotting and visualisation libraries
@@ -79,4 +82,26 @@ def plot_measure(state, shots=1000):
     plt.show()
     return fig
 
-
+def plot_measure_DMs(state, meas_shots=1000):
+    fig, ax = plt.subplots(figsize=(3, 5))
+    basis_states = {
+        '|00⟩': 0,
+        '|01⟩': 0,
+        '|10⟩': 0,
+        '|11⟩': 0
+    }
+    basis_states_noise = basis_states
+    
+    meast, ideal = state.measure_shots(shots=[meas_shots])
+    for k in basis_states:
+        k_ = k.replace('|', '').replace('⟩', '')
+        if k_ in meast:
+            basis_states[k] = float(ideal[k_])/float(meas_shots)
+            basis_states_noise[k] = float(meast[k_])/float(meas_shots)
+    ax.bar(list(basis_states.keys()), list(basis_states.values()), 0.25, label='Ideal', color='dodgerblue')
+    ax.bar(list(basis_states_noise.keys()), list(basis_states_noise.values()), 0.25, label=f'With {meas_shots} shots', color='darkorange', alpha=0.7)
+    ax.legend(frameon=0)
+    ax.set_title(f'Measurement over {meas_shots} shots')
+    ax.set_ylabel('Probability')
+    plt.show()
+    return fig
